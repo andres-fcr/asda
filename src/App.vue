5bba1098924a9880ff21df9c6e2e875a72ef86f5
Andres Cagua <andres.fcr089@gmail.com>
2023-02-14 17:45:23 -0500

    basic

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<HelloWorld v-if="modal" @modal="toggleModal" />
+	<button @mouseover="toggleModal" class="p-3 bg-slate-500 text-white">Open modal</button>
+</template>
+
+<script setup>
+import {ref} from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+const modal = ref(false);
+const toggleModal = () => {
+	modal.value = !modal.value;
+};
+</script>
+<style scoped>
+.logo {
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+	transition: filter 300ms;
+}
+.logo:hover {
+	filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+	filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+	<div ref="box">
+		<div class="bg-white rounded text-black">
+			texto de prueba {{ numero }}
+			<input type="number" name="asd" id="asd" v-model="numero" />
+			<button @mouseover="toggle">close</button>
+		</div>
+	</div>
+</template>
+<script setup>
+import {ref} from "vue";
+const numero = ref(0);
+const emit = defineEmits(["modal"]);
+
+const toggle = () => {
+	emit("modal");
+};
+</script>
+
+<style scoped>
+.read-the-docs {
+	color: #888;
+}
+</style>
